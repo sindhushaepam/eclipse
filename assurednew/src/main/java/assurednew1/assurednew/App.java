e03ae2a8cf46a17933416dcbc7687bc07e9fecbf
Sindhusha_Pericharla <Sindhusha_Pericharla@EPINHYDW0124.princeton.epam.com>
2019-08-30 17:28:40 +0530

    commit1

---
+++ assurednew/src/main/java/assurednew1/assurednew/App.java
@@ -0,0 +1,13 @@
+package assurednew1.assurednew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
